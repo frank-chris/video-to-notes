d72e3b253edfc7ac5458aad0fbd9873319a8a50c
Chris Francis <francis@repmus2.ircam.fr>
2024-05-19 13:14:07 +0200

    add files for next steps

---
+++ src/assets_from_video.py
@@ -0,0 +1,4 @@
+# write a class that will have functions to:
+# 1. read the video
+# 2. extract frames from the video that may contain diagrams/equations (use Video-Llava to do this?)
+# 3. query Video-Llava to get descriptions of the frames
---
+++ src/question_answering.py
@@ -0,0 +1,5 @@
+# write a class that will have functions to:
+# 1. read the transcript
+# 2. save embeddings of the transcript to a vector db
+# 3. query vector db with question to get relevant context
+# 4. query Llama with the relevant context and question to get the answer
